45016e8cd1ca787b39ff7dc96fa16094cf46b944
hkbarton <hkbarton@gmail.com>
2014-02-03 09:13:47 -0800

    add some note

--- install.sh
+++ install.sh
@@ -4,5 +4,4 @@
 # 3. vim-colors-solarized
 # 4. vim-commentary
 # 5. vim-powerline
-# 6. YouCompleteMe
-
+# 6. YouCompleteMe note: need run "git submodule update --init --recursive" to fetch 3rd lib
