b74011088d4eec6eee2643ac83fccfe65997c748
hkbarton <hkbarton@gmail.com>
2014-10-09 17:15:01 -0700

    update

--- install.sh
+++ install.sh
@@ -8,18 +8,50 @@
 # 6. YouCompleteMe note: need run "git submodule update --init --recursive" to fetch 3rd lib
 
 # Create temp folder for make
+LOG="install.log"
+
 mkdir temp
 
 function imac(){
+  # Install MacVim
   MacVimRepo="https://github.com/b4winckler/macvim.git"
+  HasMacVim=1
   if [ ! -z `hash mvim` ]; then
-    echo "Need install MacVim first"
+    echo "### need install MacVim first ###"
     cd temp
-    git clone $MacVimRepo
+    echo "cloneing source code of MacVim......" 
+    echo "cloneing source code of MacVim......" > $LOG
+    git clone $MacVimRepo > $LOG 2>1
     cd macvim
-    ./configure --with-features=big
-    make
-    cp MacVim/build/Release/MacVim.app /Applications/
+    echo "configuring MacVim......."
+    echo "\nconfiguring MacVim......." > $LOG
+    ./configure --with-features=huge > $LOG 2>1
+    if [ $? -eq 0 ]; then
+      echo "make MacVim......"
+      echo "\nmake MacVim......" > $LOG
+      make > $LOG 2>1
+      if [ $? -eq 0 ]; then
+        cp src/MacVim/build/Release/MacVim.app /Applications/
+        cp ../../
+        # need run using sudo
+        cp mac/mvim /usr/bin
+        echo "alias vi='mvim'" >> ~/.bash_profile
+        echo "alias vim='mvim'" >> ~/.bash_profile
+        HasMacVim=0
+        echo "### successful install MacVim ###"
+      else
+        echo "make MacVim failed, check log file"
+      fi
+    else
+      echo "configure MacVim failed, check log file"
+    fi
+  else
+    HasMacVim=0
+  fi
+
+  # Instal VIM Plugin
+  if [ HasMacVim -eq 0 ]; then
+    echo "Install vim plugin"
   fi
 }
 
